134f5e01b3325196ee3739fe1072af89c6fc6733
Johnatan Modesto <johnatan.modesto@gmail.com>
2022-01-31 10:44:43 -0300

    Removed run test from lint staged

--- web/.lintstagedrc.js
+++ web/.lintstagedrc.js
@@ -1,7 +1,6 @@
 module.exports = {
   '**/*.ts?(x)': [
     filenames => `next lint --max-warnings=0 --fix --file ${filenames.map(file => file.split(process.cwd())[1]).join(' --file ')}`,
-    "yarn lint:css --fix",
-    "yarn test --findRelatedTests --bail"
+    "yarn lint:css --fix"
   ]
 }
